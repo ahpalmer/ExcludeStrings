e3d9f9262306b1b5f637627983957ce9da7e4e35
Andrew Palmer <ahpalmer72@gmail.com>
2024-07-18 13:03:19 -0400

    Initial and final commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExcludeMachines;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ExcludeMachines();
+    }
+
+    public static void ExcludeMachines()
+    {
+        List<string> machines = Utility.CreateStringList("machineNames.txt");
+        List<string> excludeMachines = Utility.CreateStringList("excludeMachines.txt");
+        List<string> result = machines.Except(excludeMachines).ToList();
+
+        Console.WriteLine(result.Count());
+        foreach (string machine in result)
+        {
+            Console.WriteLine(machine);
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcludeMachines;
+
+public class Utility
+{
+    public static string RetrieveData(string fileName = "input.txt")
+    {
+        return File.ReadAllText(Utility.GetPath(fileName));
+    }
+
+    public static List<string> CreateStringList(string fileName = "input.txt")
+    {
+        List<string> returnList = new List<string>();
+        using (StreamReader sr = File.OpenText(Utility.GetPath(fileName)))
+        {
+            string line;
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            while ((line = sr.ReadLine()) != null)
+            {
+                returnList.Add(line);
+            }
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+        }
+        return returnList;
+    }
+
+    public static string GetPath(string fileName = "input.txt")
+    {
+        string dir = Directory.GetCurrentDirectory();
+        return dir + "\\..\\..\\..\\data\\" + fileName;
+    }
+}
